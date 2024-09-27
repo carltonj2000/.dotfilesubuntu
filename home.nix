bc5e740273d0a19649a83dc88818547b6bc50a98
Carlton Joseph <carlton.joseph@gmail.com>
2024-09-27 15:17:57 -0700

    moved zsh setup to a module

--- home.nix
+++ home.nix
@@ -73,37 +73,8 @@
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
-  
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      ll = "ls -l";
-    };
-
-    oh-my-zsh = {
-      enable = true;
-      theme = "robbyrussell";
-      plugins = [
-        "vi-mode"
-      ];
-    };
-    initExtra = ''
-      # fnm
-      FNM_PATH="/home/carltonj2000/.local/share/fnm"
-      if [ -d "$FNM_PATH" ]; then
-        export PATH="$FNM_PATH:$PATH"
-        eval "`fnm env`"
-      fi
-      . "$HOME/.cargo/env"
-      CARGO_TARGET_DIR="/home/carltonj2000/.cargo/bin"
-    '';
-  };
 
   home.file.".config/Code/User/settings.json".source = ./settings.json;
-
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,7 +1,31 @@
 {inputs, pkgs, ...}:
 
 {
-  home.file."testscript.sh".text = ''
-   echo hi
-  '';
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      ll = "ls -l";
+    };
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        "vi-mode"
+      ];
+    };
+    initExtra = ''
+      # fnm
+      FNM_PATH="/home/carltonj2000/.local/share/fnm"
+      if [ -d "$FNM_PATH" ]; then
+        export PATH="$FNM_PATH:$PATH"
+        eval "`fnm env`"
+      fi
+      . "$HOME/.cargo/env"
+      CARGO_TARGET_DIR="/home/carltonj2000/.cargo/bin"
+    '';
+  };
 }
